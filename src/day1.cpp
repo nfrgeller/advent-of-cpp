bd6481e19305d9e6ac7f5f1fbfc7c94cb8b0614e
nfrgeller <69528923+nfrgeller@users.noreply.github.com>
2025-12-01 19:38:08 -0600

    day1 part 1 correct

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,8 +1,32 @@
 #include "utils.hpp"
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
   std::string puzzle_input = utils::read_puzzle_input(1);
-  std::cout << puzzle_input << std::endl;
+  std::istringstream stream(puzzle_input);
+  std::string current_line;
+
+  int location = 50;
+  int result = 0;
+
+  while (std::getline(stream, current_line)) {
+    char direction = current_line[0];
+    int distance = std::stoi(current_line.substr(1));
+
+    if (direction == 'L') {
+      distance *= -1;
+    }
+
+    location = (location + distance) % 100;
+
+    if (location == 0) {
+      result++;
+    }
+  }
+
+  std::cout << result << std::endl;
+
+  return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,27 +4,24 @@
 #include <fstream>
 #include <stdexcept>
 
-namespace utils
-{
+namespace utils {
 
-  std::string read_puzzle_input(int day)
-  {
+std::string read_puzzle_input(int day) {
 
-    std::string input_file_name = std::format("day{}-input.txt", day);
+  std::string input_file_name = std::format("day{}-input.txt", day);
 
-    std::filesystem::path input_file_path =
-        std::filesystem::path("puzzle-inputs") / input_file_name;
+  std::filesystem::path input_file_path =
+      std::filesystem::path("puzzle-inputs") / input_file_name;
 
-    if (!std::filesystem::exists(input_file_path))
-    {
-      throw std::runtime_error("File not found: " + input_file_path.string());
-    }
-
-    std::ifstream input_file(input_file_path);
-
-    std::string content((std::istreambuf_iterator<char>(input_file)),
-                        std::istreambuf_iterator<char>());
-    return content;
+  if (!std::filesystem::exists(input_file_path)) {
+    throw std::runtime_error("File not found: " + input_file_path.string());
   }
 
+  std::ifstream inputFile(input_file_path);
+
+  std::string content((std::istreambuf_iterator<char>(inputFile)),
+                      std::istreambuf_iterator<char>());
+  return content;
 }
+
+} // namespace utils
