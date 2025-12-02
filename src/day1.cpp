2df85e7d2d53645a0dada2df77f5f606d8cb85e0
nfrgeller <69528923+nfrgeller@users.noreply.github.com>
2025-12-02 10:14:46 -0600

    Finished day 1 part 2.

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,9 +1,9 @@
-#include "utils.hpp"
+#include "utils.h"
 #include <iostream>
 #include <sstream>
 #include <string>
 
-int main() {
+int part1() {
   std::string puzzle_input = utils::read_puzzle_input(1);
   std::istringstream stream(puzzle_input);
   std::string current_line;
@@ -19,14 +19,47 @@ int main() {
       distance *= -1;
     }
 
-    location = (location + distance) % 100;
+    int total = location + distance;
 
+    location = total % 100;
     if (location == 0) {
       result++;
     }
   }
+  return result;
+}
 
-  std::cout << result << std::endl;
+int part2() {
+  std::string puzzle_input = utils::read_puzzle_input(1);
+  std::istringstream stream(puzzle_input);
+  std::string current_line;
 
-  return 0;
+  int location = 50;
+  int result = 0;
+
+  while (std::getline(stream, current_line)) {
+    char direction = current_line[0];
+    int distance = std::stoi(current_line.substr(1));
+    int i = location;
+    int count = 0;
+
+    while (count < distance) {
+      if (direction == 'L') {
+        i = (i - 1 + 100) % 100;
+      } else {
+        i = (i + 1) % 100;
+      }
+      if (i == 0) {
+        result++;
+      }
+      location = i;
+      count++;
+    }
+  }
+  return result;
+}
+
+int main() {
+  std::cout << part1() << std::endl;
+  std::cout << part2() << std::endl;
 }
